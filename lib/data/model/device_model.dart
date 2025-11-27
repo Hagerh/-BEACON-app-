7e0eff0e7a56481ed2c5875599109eb177e8532b
Siveen3 <127445863+Siveen3@users.noreply.github.com>
2025-11-27 15:51:23 +0200

    Add from and to db map functions

--- lib/data/model/device_model.dart
+++ lib/data/model/device_model.dart
@@ -11,4 +11,29 @@ class Device {
     required this.status,
     required this.connectors,
   });
+
+  factory Device.fromMap(Map<String, dynamic> m) {
+    return Device(
+      id: m['id']?.toString()
+          ?? (m['device_id']?.toString())
+          ?? (m['network_name']?.toString())
+          ?? '',
+      status: m['status']?.toString()
+          ?? m['host_status']?.toString()
+          ?? 'Unknown',
+      lastSeen: m['last_seen_at']?.toString() ?? (m['lastSeen']?.toString() ?? ''),
+      connectors: (m['connectors'] is int)
+          ? m['connectors'] as int
+          : int.tryParse(m['connectors']?.toString() ?? '0') ?? 0,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'status': status,
+      'last_seen_at': lastSeen,
+      'connectors': connectors,
+    };
+  }
 }
