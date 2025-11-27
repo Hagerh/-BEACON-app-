d0fea8a76506985f40e304f67c7c0f31c6ce6bcd
Siveen3 <127445863+Siveen3@users.noreply.github.com>
2025-11-27 15:46:06 +0200

    Calls from DB

--- lib/business/cubit/network_cubit.dart
+++ lib/business/cubit/network_cubit.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:projectdemo/data/model/device_model.dart';
 import 'package:projectdemo/business/cubit/network_state.dart';
+import 'package:projectdemo/data/local/database_helper.dart';
 
 class NetworkCubit extends Cubit<NetworkState> {
   NetworkCubit() : super(NetworkInitial()); // Initial state
@@ -11,27 +12,13 @@ class NetworkCubit extends Cubit<NetworkState> {
   Future<void> loadNetworks() async {
     
      emit(NetworkLoading());
-
-    // Delay to simulate network/p2p discovery time
-    await Future.delayed(const Duration(milliseconds: 500)); 
-
-    // TODO: Replace with real peer-to-peer  logic 
-    final List<Device> initialNetworks = [
-      Device(
-        id: "Emergency hub 01",
-        status: "Connected",
-        lastSeen: "2 mins ago",
-        connectors: 1,
-      ),
-      Device(
-        id: "Available Network 02",
-        status: "Available",
-        lastSeen: "5 mins ago",
-        connectors: 3,
-      ),
-    ];
-    
-    emit(NetworkLoaded(networks: initialNetworks));
+    try {
+      // fetch from local sqlite
+      final networks = await DatabaseHelper.instance.fetchNetworkSummaries();
+      emit(NetworkLoaded(networks: networks));
+    } catch (e) {
+      emit(NetworkError(e.toString()));
+    }
   }
 
   // Simulates refreshing the network list
@@ -41,35 +28,12 @@ class NetworkCubit extends Cubit<NetworkState> {
       final currentState = state as NetworkLoaded;
       
       emit(currentState.copyWith(isRefreshing: true));
-
-      
-      await Future.delayed(const Duration(seconds: 2));
-
-      // TODO: Replace with real peer-to-peer  logic 
-      final List<Device> refreshedNetworks = [
-        Device(
-          id: "Emergency hub 01",
-          status: "Connected",
-          lastSeen: "just now",
-          connectors: 1,
-        ),
-        Device(
-          id: "Available Network 03",
-          status: "Available",
-          lastSeen: "10 seconds ago",
-          connectors: 2,
-        ),
-        // A new device appears
-        Device(
-          id: "New Device Found 04",
-          status: "Available",
-          lastSeen: "just now",
-          connectors: 5,
-        ),
-      ];
-      
-      // Emit the final loaded state with isRefreshing set to false
-      emit(NetworkLoaded(networks: refreshedNetworks, isRefreshing: false));
+      try {
+        final networks = await DatabaseHelper.instance.fetchNetworkSummaries();
+        emit(NetworkLoaded(networks: networks, isRefreshing: false));
+      } catch (e) {
+        emit(NetworkError(e.toString()));
+      }
     }
   }
 }
