9cfa6c547c1cf9a7b9ce49b77da7a85a90aa8af1
Siveen3 <127445863+Siveen3@users.noreply.github.com>
2025-11-27 15:52:24 +0200

    Add from and to db map functions

--- lib/data/model/message_model.dart
+++ lib/data/model/message_model.dart
@@ -1,9 +1,10 @@
+
 import 'package:flutter/material.dart';
 
 class Message {
   final String text;
   final bool isMine;
-  final TimeOfDay time; 
+  final TimeOfDay time;
   final bool isDelivered;
 
   Message({
@@ -20,6 +21,24 @@ class Message {
       isMine: isMine,
       time: time,
       isDelivered: isDelivered ?? this.isDelivered,
+    );
+  }
+
+  factory Message.fromMap(Map<String, dynamic> m) {
+    TimeOfDay time = TimeOfDay.now();
+    if (m['sent_at'] != null) {
+      try {
+        final dt = DateTime.parse(m['sent_at'].toString());
+        time = TimeOfDay(hour: dt.hour, minute: dt.minute);
+      } catch (_) {}
+    }
+    final isMine = (m['is_mine'] is int) ? (m['is_mine'] as int) == 1 : m['is_mine'] == true;
+    final isDelivered = (m['is_delivered'] is int) ? (m['is_delivered'] as int) == 1 : m['is_delivered'] == true;
+    return Message(
+      text: m['message_content']?.toString() ?? '',
+      isMine: isMine,
+      time: time,
+      isDelivered: isDelivered,
     );
   }
 }
